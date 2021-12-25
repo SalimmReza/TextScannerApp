ac0bc0224b1ae00e2c2adbc345048c6882323562
SalimmReza <refath300@gmail.com>
2021-12-25 14:15:13 +0600

    done

--- app/src/main/java/com/example/textscannerapp/MainActivity.java
+++ app/src/main/java/com/example/textscannerapp/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -15,10 +17,15 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -57,6 +64,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        copy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String scan_text= data.getText().toString();
+                copy_to_clip_board(scan_text);
+            }
+        });
+
 
     }
 
@@ -71,12 +87,44 @@ public class MainActivity extends AppCompatActivity {
                 Uri result_uri = result.getUri();
                 try {
                     bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), result_uri);
-
+                    get_text_from_image(bitmap);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
 
             }
         }
+    }
+
+    private void get_text_from_image(Bitmap bitmap)
+    {
+        TextRecognizer recognizer = new TextRecognizer.Builder(this).build();
+        if (!recognizer.isOperational())
+        {
+            Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+        }else
+        {
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> textBlockSparseArray = recognizer.detect(frame);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i =0 ; i<textBlockSparseArray.size(); i++)
+            {
+                TextBlock textBlock = textBlockSparseArray.valueAt(i);
+                stringBuilder.append(textBlock.getValue());
+                stringBuilder.append("\n");
+
+            }
+            data.setText(stringBuilder.toString());
+            capture.setText("Retake");
+            copy.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void copy_to_clip_board(String text)
+    {
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clipData = ClipData.newPlainText("Copied Data", text);
+        clipboardManager.setPrimaryClip(clipData);
+        Toast.makeText(MainActivity.this, "Copied", Toast.LENGTH_SHORT).show();
     }
 }
